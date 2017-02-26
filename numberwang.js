61c53db8a82b25f559b4db331dcac3ce480af036
Gilmore Davidson <gilmoreorless@gmail.com>
2017-02-27 07:29:08 +1100

    Mostly whitespace change - wrapped in an IIFE

--- numberwang.js
+++ numberwang.js
@@ -1,96 +1,101 @@
-var IS_TESTING = document.documentElement.hasAttribute('data-numberwang-testing');
+(function () {
 
-var curCount, targetCount;
-var nwCount, targetNw;
+	var IS_TESTING = document.documentElement.hasAttribute('data-numberwang-testing');
 
-var elemValueCache = new WeakMap();
+	var curCount, targetCount;
+	var nwCount, targetNw;
 
-function trigger(name, elem, value) {
-	var event = new CustomEvent(`numberwang-${name}`, {
-		detail: value,
-		bubbles: true
-	});
-	(elem || document).dispatchEvent(event);
-}
+	var elemValueCache = new WeakMap();
 
-function resetCount() {
-	curCount = 0;
-	targetCount = Math.floor(Math.random() * 20) + 2;
-}
-
-function resetNwCount() {
-	nwCount = 0;
-	targetNw = Math.floor(Math.random() * 3) + 3;
-}
-
-function arraysAreEqual(arr1, arr2) {
-	if (!arr1 || !arr2) return false;
-	if (arr1.length !== arr2.length) return false;
-	var hasDiff = arr1.some((n, i) => arr2[i] !== n);
-	return !hasDiff;
-}
-
-function arrayDiff(arr1, arr2) {
-	return arr1.filter((n, i) => arr2[i] !== n);
-}
-
-function extractNumbers(str) {
-	var numbers = String(str).trim()
-		.split(/[^0-9.\-]+/)
-		.map(n => parseFloat(n))
-		.filter(n => !isNaN(n));
-	return numbers;
-}
-
-function parseElement(elem) {
-	var numbers = extractNumbers(elem.value);
-	if (!numbers.length) {
-		return;
+	function trigger(name, elem, value) {
+		var event = new CustomEvent(`numberwang-${name}`, {
+			detail: value,
+			bubbles: true
+		});
+		(elem || document).dispatchEvent(event);
 	}
-	var existing = elemValueCache.get(elem) || [];
-	if (arraysAreEqual(numbers, existing)) {
-		return;
-	}
-	elemValueCache.set(elem, numbers);
 
-	var diff = arrayDiff(numbers, existing);
-	diff.forEach(n => {
-		checkNumberwang(elem, n);
-	});
-}
-
-function checkNumberwang(elem, number) {
-	curCount++;
-	trigger('check', elem, { number });
-	if (curCount === targetCount) {
-		thatsNumberwang(elem, number);
+	function resetCount() {
+		curCount = 0;
+		targetCount = Math.floor(Math.random() * 20) + 2;
 	}
-}
 
-function thatsNumberwang(elem, number) {
-	nwCount++;
-	var rotate = nwCount === targetNw;
-	trigger('found', elem, { number, count: nwCount, rotate });
-	if (rotate) {
-		resetNwCount();
+	function resetNwCount() {
+		nwCount = 0;
+		targetNw = Math.floor(Math.random() * 3) + 3;
 	}
+
+	function arraysAreEqual(arr1, arr2) {
+		if (!arr1 || !arr2) return false;
+		if (arr1.length !== arr2.length) return false;
+		var hasDiff = arr1.some((n, i) => arr2[i] !== n);
+		return !hasDiff;
+	}
+
+	function arrayDiff(arr1, arr2) {
+		return arr1.filter((n, i) => arr2[i] !== n);
+	}
+
+	function extractNumbers(str) {
+		var numbers = String(str).trim()
+			.split(/[^0-9.\-]+/)
+			.map(n => parseFloat(n))
+			.filter(n => !isNaN(n));
+		return numbers;
+	}
+
+	function parseElement(elem) {
+		var numbers = extractNumbers(elem.value);
+		if (!numbers.length) {
+			return;
+		}
+		var existing = elemValueCache.get(elem) || [];
+		if (arraysAreEqual(numbers, existing)) {
+			return;
+		}
+		elemValueCache.set(elem, numbers);
+
+		var diff = arrayDiff(numbers, existing);
+		diff.forEach(n => {
+			checkNumberwang(elem, n);
+		});
+	}
+
+	function checkNumberwang(elem, number) {
+		curCount++;
+		trigger('check', elem, { number });
+		if (curCount === targetCount) {
+			thatsNumberwang(elem, number);
+		}
+	}
+
+	function thatsNumberwang(elem, number) {
+		nwCount++;
+		var rotate = nwCount === targetNw;
+		trigger('found', elem, { number, count: nwCount, rotate });
+		if (rotate) {
+			resetNwCount();
+		}
+		resetCount();
+	}
+
 	resetCount();
-}
+	resetNwCount();
 
-resetCount();
-resetNwCount();
-
-document.addEventListener('input', function (e) {
-	var elem = e.target;
-	setTimeout(() => {
-		parseElement(elem);
-	});
-}, false);
+	document.addEventListener('input', function (e) {
+		var elem = e.target;
+		setTimeout(() => {
+			parseElement(elem);
+		});
+	}, false);
 
 
-///// INTERACTIONS
+	///// INTERACTIONS
 
-if (!IS_TESTING) {
+	if (IS_TESTING) {
+		return;
+	}
+
 	var cssClasses = {
 		ready: 'numberwang-ready',
 		rotate: 'numberwang-rotate-the-board',
@@ -136,4 +141,5 @@ if (!IS_TESTING) {
 			document.body.classList.add(cssClasses.rotate);
 		}
 	});
-}
+
+})();
